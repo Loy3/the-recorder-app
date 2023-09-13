6192454bafc38205300c64187156125774400198
Loy3 <netshiozwiloy@gmail.com>
2023-09-13 15:54:39 +0200

    13/09/2023

--- App.js
+++ App.js
@@ -89,13 +89,52 @@ export default function App({ navigation }) {
       const jsonValue = await AsyncStorage.getItem('user');
       // const jsonValue = await AsyncStorage.removeItem('user');
 
-      // const user = JSON.parse(jsonValue);
+      const user = JSON.parse(jsonValue);
       // console.log("RTN User", JSON.parse(jsonValue));
+      const token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjE5MGFkMTE4YTk0MGFkYzlmMmY1Mzc2YjM1MjkyZmVkZThjMmQwZWUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vbmZ0LWNvbGxlY3Rpb25zLTdmMTQzIiwiYXVkIjoibmZ0LWNvbGxlY3Rpb25zLTdmMTQzIiwiYXV0aF90aW1lIjoxNjk0NjA5NzU2LCJ1c2VyX2lkIjoiWU5QVmNGZEcyY1R5WnhIa0tmem9CUnk2QjBTMiIsInN1YiI6IllOUFZjRmRHMmNUeVp4SGtLZnpvQlJ5NkIwUzIiLCJpYXQiOjE2OTQ2MDk3NTYsImV4cCI6MTY5NDYxMzM1NiwiZW1haWwiOiJuZXRzaGlvendpQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJuZXRzaGlvendpQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.dpja64ESTSDGTyL-WYGz7ZufTVHKtyu5t4kqtlyt38GYbCecTvfrIOXd8DkR7m8V2wS-fJIEThjESpXID9cL3rRu4jHl-XAM2MzfkpBAoxfMwxRQTJ6c7vRZr7zyTYkUv3VYyufa76cXaOE19jxq7xVcL-bi7Zh7_3_oOm3RpPadXdcLNw5mT7UgEk0fmTtjkgpLOzZgZaP1KWsw1el8Qb7wBiApfXw7X5WulYpSwrmsHt-xkbrYfwsWEg6PtH7ygClAqgR7hzo7xM5VRzJ6guuAYiL9pA2LfmDeMC6ppVVbIzBCww4I7v-AC0bRZVdHPhTvoBsD2RU-Z2S1JY5wGw"
+      // console.log(user.expiresIn);
+      checkTokenExp(token,user.expiresIn).then((isValid) => {
+        if (isValid) {
+          console.log("My brother we are moving");
+        } else {
+          console.log("Token invalid");
+        }
+      }).catch((error) => {
+        console.log("Error", error);
+      })
+
       return jsonValue != null ? JSON.parse(jsonValue) : null;
       // return null;
     } catch (e) {
       console.log(e.message);
     }
+  }
+
+  async function checkTokenExp(token,expiresIn) {
+    const api_key = "";
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBZP4kNQQ3P64kJDMZit_blizapfoBzLas`;
+const idToken = token;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { 'ContentType': 'application/json' },
+      body: JSON.stringify({ idToken }),
+    });
+    if (response.ok) {
+      const data = await response.json();
+      const user = data.users[0];
+      console.log("===", user, data);
+      if (user) {
+        // console.log("the user", user);
+        const expTime = user.validSince + expiresIn;
+        const now = Math.floor(Date.now() / 1000);
+        console.log("exp", expTime, "now", now );
+        if (expTime > now) {
+          console.log("Still Valid");
+          return true
+        }
+      }
+    }
+    return false;
   }
 
   return (
