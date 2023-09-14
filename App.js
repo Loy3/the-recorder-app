ebf26eef4540cdb748e343dd496343197085a203
Loy3 <netshiozwiloy@gmail.com>
2023-09-14 07:20:51 +0200

    14/09/2023

--- App.js
+++ App.js
@@ -65,7 +65,7 @@ export default function App({ navigation }) {
 
     (async () => {
       const user = await getUser();
-      // console.log("done");
+      // console.log("done", user);
       if (user !== null) {
         // console.log("user",user.email)
         setUserMail(user.email)
@@ -90,30 +90,38 @@ export default function App({ navigation }) {
       // const jsonValue = await AsyncStorage.removeItem('user');
 
       const user = JSON.parse(jsonValue);
-      // console.log("RTN User", JSON.parse(jsonValue));
-      const token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjE5MGFkMTE4YTk0MGFkYzlmMmY1Mzc2YjM1MjkyZmVkZThjMmQwZWUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vbmZ0LWNvbGxlY3Rpb25zLTdmMTQzIiwiYXVkIjoibmZ0LWNvbGxlY3Rpb25zLTdmMTQzIiwiYXV0aF90aW1lIjoxNjk0NjA5NzU2LCJ1c2VyX2lkIjoiWU5QVmNGZEcyY1R5WnhIa0tmem9CUnk2QjBTMiIsInN1YiI6IllOUFZjRmRHMmNUeVp4SGtLZnpvQlJ5NkIwUzIiLCJpYXQiOjE2OTQ2MDk3NTYsImV4cCI6MTY5NDYxMzM1NiwiZW1haWwiOiJuZXRzaGlvendpQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJuZXRzaGlvendpQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.dpja64ESTSDGTyL-WYGz7ZufTVHKtyu5t4kqtlyt38GYbCecTvfrIOXd8DkR7m8V2wS-fJIEThjESpXID9cL3rRu4jHl-XAM2MzfkpBAoxfMwxRQTJ6c7vRZr7zyTYkUv3VYyufa76cXaOE19jxq7xVcL-bi7Zh7_3_oOm3RpPadXdcLNw5mT7UgEk0fmTtjkgpLOzZgZaP1KWsw1el8Qb7wBiApfXw7X5WulYpSwrmsHt-xkbrYfwsWEg6PtH7ygClAqgR7hzo7xM5VRzJ6guuAYiL9pA2LfmDeMC6ppVVbIzBCww4I7v-AC0bRZVdHPhTvoBsD2RU-Z2S1JY5wGw"
-      // console.log(user.expiresIn);
-      checkTokenExp(token,user.expiresIn).then((isValid) => {
-        if (isValid) {
-          console.log("My brother we are moving");
-        } else {
-          console.log("Token invalid");
-        }
-      }).catch((error) => {
-        console.log("Error", error);
-      })
+      console.log("RTN User", JSON.parse(jsonValue));
+      if (user !== null) {
+        const token = user.idToken;
 
+        // console.log(user.expiresIn);
+        checkTokenExp(token, user.expiresIn).then(async (isValid) => {
+          if (isValid) {
+            // console.log("My brother we are moving");
+            // setSignIn(true);
+            return jsonValue != null ? JSON.parse(jsonValue) : null;
+          } else {
+            console.log("Token invalid");
+            // await AsyncStorage.removeItem('user');
+            // setSignIn(false);
+            return null;
+          }
+        }).catch((error) => {
+          console.log("Error", error);
+        })
+      }
       return jsonValue != null ? JSON.parse(jsonValue) : null;
+
       // return null;
     } catch (e) {
       console.log(e.message);
     }
   }
 
-  async function checkTokenExp(token,expiresIn) {
+  async function checkTokenExp(token, expiresIn) {
     const api_key = "";
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBZP4kNQQ3P64kJDMZit_blizapfoBzLas`;
-const idToken = token;
+    const idToken = token;
     const response = await fetch(url, {
       method: "POST",
       headers: { 'ContentType': 'application/json' },
@@ -127,7 +135,7 @@ const idToken = token;
         // console.log("the user", user);
         const expTime = user.validSince + expiresIn;
         const now = Math.floor(Date.now() / 1000);
-        console.log("exp", expTime, "now", now );
+        console.log("exp", expTime, "now", now);
         if (expTime > now) {
           console.log("Still Valid");
           return true
